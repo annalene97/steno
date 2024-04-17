bd5a99b1a7390d4ecfb169b2748c86c94bdec5f9
Nikoline01 <146435826+Nikoline01@users.noreply.github.com>
2024-04-17 15:48:29 +0200

    hej

--- script.js
+++ script.js
@@ -26,4 +26,46 @@ document.addEventListener("DOMContentLoaded", function() {
     var introTekst = document.querySelector('.intro-tekst');
     introTekst.classList.add('fade-out');
   });
-  
+
+  //Velkomst tekst fader ind, når play aktiveres//
+document.getElementById('videoPlayer').addEventListener('play', function() {
+  var introText = document.querySelector('.velkomst-tekst');
+  introText.classList.add('fade-in');
+});
+
+
+//Typewriting effekt på velkomst tekst//
+var i = 0;
+var txt = 'Hej med dig!! Jeg vil tage dig med på en rejse gennem tiden og vise dig alt fra fyldige former i renæssancen til slanke silhuetter i 1990erne.';
+var speed = 60;
+var paragraph = document.getElementById("demo");
+
+function typeWriter(callback) {
+  if (i < txt.length) {
+    paragraph.textContent += txt.charAt(i);
+    i++;
+    setTimeout(function() {
+      typeWriter(callback);
+    }, speed);
+  } else {
+    if (callback) {
+      callback();
+    }
+  }
+  document.getElementById("demo2").style.display = "none";
+}
+
+//Gemmer den første del af teksten, så den resterende velkomst tekst kommer frem//
+document.getElementById("playKnap").addEventListener("click", function() {
+  typeWriter(function() {
+    document.getElementById("demo").style.display = "none";
+    txt = "Så træd ind i en verden af skønhed, form og kultur og undersøg, sammen med mig, hvordan fortidens idealer møder nutidens realiteter.";
+    i = 0;
+    document.getElementById("demo2").style.display = "block";
+    typeWriter();
+  });
+});
+
+
+
+
