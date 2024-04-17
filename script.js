6d67a3a3358ab84d2fa5ce5f88acd58e504d57ea
annalene97 <annalenerasmussen@outlook.com>
2024-04-17 15:46:22 +0200

    123

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Afspil video ved klik af play-knap//
-document.addEventListener("DOMContentLoaded", function() {
+/* document.addEventListener("DOMContentLoaded", function() {
     var video = document.getElementById("videoPlayer");
     var playKnap = document.getElementById("playKnap");
   
@@ -19,11 +19,23 @@ document.addEventListener("DOMContentLoaded", function() {
     video.addEventListener("pause", function() {
       playKnap.style.display = "block";
     });
-  });
+  }); */
   
 //Intro tekst fader ud, ved aktivering af play knap)
-  document.getElementById('playKnap').addEventListener('click', function() {
+/*   document.getElementById('playKnap').addEventListener('click', function() {
     var introTekst = document.querySelector('.intro-tekst');
     introTekst.classList.add('fade-out');
   });
-  
+   */
+
+  //Afstemningsboks
+  var svarKnap = document.getElementById("knap_svar");
+  var afstemningFoer = document.getElementById("afstemning_svar");
+  var afstemningEfter = document.getElementById("afstemning_valg");
+
+  svarKnap.addEventListener("click", function() {
+        afstemningFoer.style.display = "none";
+        afstemningEfter.style.display = "flex";
+  });
+
+
