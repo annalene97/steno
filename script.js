eb6f2ed1c5294ae01b4b48e462264441296a445d
yde-emma <145010724+yde-emma@users.noreply.github.com>
2024-04-18 09:45:18 +0200

    1950 opdateret

--- script.js
+++ script.js
@@ -67,21 +67,12 @@ document.getElementById("playKnap").addEventListener("click", function() {
 });
 
 
-<<<<<<< Updated upstream
 //Afstemningsboks
 var svarKnap = document.getElementById("knap_svar");
 var afstemningFoer = document.getElementById("afstemning_svar");
 var afstemningEfter = document.getElementById("afstemning_valg");
 
 svarKnap.addEventListener("click", function() {
-=======
-  //Afstemningsboks
-  var svarKnap = document.getElementById("knap_svar");
-  var afstemningFoer = document.getElementById("afstemning_svar");
-  var afstemningEfter = document.getElementById("afstemning_valg");
-
-  svarKnap.addEventListener("click", function() {
->>>>>>> Stashed changes
         afstemningFoer.style.display = "none";
         afstemningEfter.style.display = "flex";
 });
