b953f2f8a5bfe23f2c61b4898aa834002786c0ca
Nikoline01 <146435826+Nikoline01@users.noreply.github.com>
2024-04-18 13:41:18 +0200

    hej

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-//Afspil video ved klik af play-knap//
-    document.addEventListener("DOMContentLoaded", function() {
-    var video = document.getElementById("videoPlayer");
-    var playKnap = document.getElementById("playKnap");
-  
-    playKnap.addEventListener("click", function() {
-      if (video.paused) {
-        video.play();
-        playKnap.style.display = "none";
-      } else {
-        video.pause();
-      }
-    });
-  
-    video.addEventListener("play", function() {
+//Afspil video ved klik af play-knap///*
+document.addEventListener("DOMContentLoaded", function() {
+  var video = document.getElementById("videoPlayer");
+  var playKnap = document.getElementById("playKnap");
+
+  playKnap.addEventListener("click", function() {
+    if (video.paused) {
+      video.play();
       playKnap.style.display = "none";
-    });
-  
-    video.addEventListener("pause", function() {
-      playKnap.style.display = "block";
-    });
+    } else {
+      video.pause();
+    }
   });
+
+  video.addEventListener("play", function() {
+    playKnap.style.display = "none"; // Gem play knap, når videoen starter
+  });
+
+  video.addEventListener("pause", function() {
+    playKnap.style.display = "none"; // Pause knappen forbliver gemt væk når videoen stoppes
+  });
+});
+
   
 //Intro tekst fader ud, ved aktivering af play knap)
     document.getElementById('playKnap').addEventListener('click', function() {
@@ -35,56 +36,103 @@
 
 
 //Typewriting effekt på velkomst tekst//
+var initialTxt = 'Hej med dig!! Jeg vil tage dig med på en rejse gennem tiden og vise dig alt fra fyldige former i renæssancen til slanke silhuetter i 1990erne.';
+var replacementTxt = 'Så træd ind i en verden af skønhed, form og kultur og undersøg, sammen med mig, hvordan fortidens idealer møder nutidens realiteter.';
+var speed = 50;
+var pauseDuration = 2000;
 var i = 0;
-var txt = 'Hej med dig!! Jeg vil tage dig med på en rejse gennem tiden og vise dig alt fra fyldige former i renæssancen til slanke silhuetter i 1990erne.';
-var speed = 60;
-var paragraph = document.getElementById("demo");
 
+// Function til typewriting effekt
 function typeWriter(callback) {
-  if (i < txt.length) {
-    paragraph.textContent += txt.charAt(i);
+  if (i < initialTxt.length) {
+    document.getElementById("demo").innerHTML += initialTxt.charAt(i);
     i++;
     setTimeout(function() {
       typeWriter(callback);
     }, speed);
   } else {
-    if (callback) {
-      callback();
-    }
+    setTimeout(callback, pauseDuration); // Pause mellem hver tekst del (hvor tiden er angivet i øverste del med pauseDuration)
   }
-  document.getElementById("demo2").style.display = "none";
-} 
+}
 
-//Gemmer den første del af teksten, så den resterende velkomst tekst kommer frem
-document.getElementById("playKnap").addEventListener("click", function() {
+//Indsæt ny tekst, når den første del er skrevet op, med id nr. 2
+function replaceText() {
+  document.getElementById("demo").innerHTML = '';
+  initialTxt = replacementTxt;
+  i = 0;
   typeWriter(function() {
-    document.getElementById("demo").style.display = "none";
-    txt = "Så træd ind i en verden af skønhed, form og kultur og undersøg, sammen med mig, hvordan fortidens idealer møder nutidens realiteter.";
-    i = 0;
-    document.getElementById("demo2").style.display = "block";
-    typeWriter();
+    console.log("Text replacement is complete.");
   });
-});
+}
 
+// Autoplay typewriting effekt når siden indlæses
+window.onload = function() {
+  typeWriter(replaceText);
+};
 
+/*
 //Afstemningsboks
 var svarKnap = document.getElementById("knap_svar");
 var afstemningFoer = document.getElementById("afstemning_svar");
 var afstemningEfter = document.getElementById("afstemning_valg");
 
 svarKnap.addEventListener("click", function() {
-<<<<<<< Updated upstream
-=======
   //Afstemningsboks
   var svarKnap = document.getElementById("knap_svar");
   var afstemningFoer = document.getElementById("afstemning_svar");
   var afstemningEfter = document.getElementById("afstemning_valg");
 
   svarKnap.addEventListener("click", function() {
->>>>>>> Stashed changes
         afstemningFoer.style.display = "none";
         afstemningEfter.style.display = "flex";
 });
 
 //Diagram modal - Modalen skal åbnes ved tryk på vilkårlig knap i afstemningEfter. Her skal funktionen både åbne modalen og reset afstemningsboksen til afstemningFoer.
 
+
+
+//Autoplay med forsinkelse på outro video
+const myTimeout = setTimeout(playVideo, 19000);
+
+function playVideo() {
+  var video = document.getElementById("videoPlayer-outro");
+  video.play();
+}
+
+
+//Typewriting effekt på outro tekst
+var initialTxt = 'Nu er vi kommet til rejsens slutning, og du ved nu lidt mere om kropsidealer gennem tiden og hvilke samfundsmæssige faktorer, som kan spille ind i vores tanker og syn på kroppen.';
+var replacementTxt = 'Som du kan se har kropsidealer ændret sig meget gennem tiden. De er altid formet af, hvordan samfundet ser ud i den pågældende tid. Alle kroppe er unikke og sammensat af forskellige gener, størrelser og former.';
+var speed = 50;
+var pauseDuration = 3000;
+var i = 0;
+
+// Function til typewriting effekt
+function typeWriter(callback) {
+  if (i < initialTxt.length) {
+    document.getElementById("demo_outro").innerHTML += initialTxt.charAt(i);
+    i++;
+    setTimeout(function() {
+      typeWriter(callback);
+    }, speed);
+  } else {
+    setTimeout(callback, pauseDuration); // Pause mellem hver tekst del (hvor tiden er angivet i øverste del med pauseDuration)
+  }
+}
+
+//Indsæt ny tekst, når den første del er skrevet op, med id nr. 2
+function replaceText() {
+  document.getElementById("demo_outro").innerHTML = '';
+  initialTxt = replacementTxt;
+  i = 0;
+  typeWriter(function() {
+    console.log("Text replacement is complete.");
+  });
+}
+
+// Autoplay typewriting effekt når siden indlæses
+window.onload = function() {
+  typeWriter(replaceText);
+};
+
+*/
